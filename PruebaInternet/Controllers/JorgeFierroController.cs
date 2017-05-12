9bbf5d3f600017f5142a65bc00571850c5a28c67
Bellavista <bellavista@labbellavista.aiep.corp>
2017-05-12 11:13:53 -0300

    agregue un nuevo controlador con su vista index

---
+++ PruebaInternet/Controllers/JorgeFierroController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PruebaInternet.Controllers
+{
+    public class JorgeFierroController : Controller
+    {
+        //
+        // GET: /JorgeFierro/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
