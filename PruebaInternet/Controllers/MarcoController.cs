858bf4a6c4dd5df02f29a83e88658650e8216175
Bellavista_Lab102 <Bellavista_Lab102@labbellavista.aiep.corp>
2017-05-12 12:38:21 -0300

    controlador marco

---
+++ PruebaInternet/Controllers/MarcoController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PruebaInternet.Controllers
+{
+    public class MarcoController : Controller
+    {
+        //
+        // GET: /Marco/
+
+        public ActionResult Index()
+        {
+
+            Console.WriteLine("Ahi te va tu controler putito");
+
+            return View();
+        }
+
+    }
+}
