619941c5e27c8d76c28e21be20e2933e6399b4b8
Bellavista_Lab102 <Bellavista_Lab102@labbellavista.aiep.corp>
2017-05-12 13:42:52 -0300

    Controlador matt qlo

---
+++ PruebaInternet/Controllers/Matt_Aravena_qloController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PruebaInternet.Controllers
+{
+    public class Matt_Aravena_qloController : Controller
+    {
+        //
+        // GET: /Matt_Aravena_qlo/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
