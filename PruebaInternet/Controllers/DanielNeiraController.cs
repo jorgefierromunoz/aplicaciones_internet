49c812ad3df04d56c34a579361a0d1f6a7610ebc
danillua <daniel6942101@hotmail.com>
2017-05-19 12:31:23 -0400

    controlador Daniel Neira

---
+++ PruebaInternet/Controllers/DanielNeiraController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PruebaInternet.Controllers
+{
+    public class DanielNeiraController : Controller
+    {
+        //
+        // GET: /DanielNeira/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
