358ab8c09d3e856be6f3744af646bddc8945e48e
Cashupin <jalvarezc96@gmail.com>
2017-05-21 23:20:23 -0400

    JuanAlvarezCController

---
+++ PruebaInternet/Controllers/JuanAlvarezCController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PruebaInternet.Controllers
+{
+    public class JuanAlvarezCController : Controller
+    {
+        //
+        // GET: /JuanAlvarezC/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
