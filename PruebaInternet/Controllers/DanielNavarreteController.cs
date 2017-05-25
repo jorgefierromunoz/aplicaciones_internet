c2c28db62b4effe994d1385ab3387359ad0371f4
Daniel <daniel21nvs@gmail.com>
2017-05-25 17:36:23 -0400

    cree nuevo controlador

---
+++ PruebaInternet/Controllers/DanielNavarreteController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PruebaInternet.Controllers
+{
+    public class DanielNavarreteController : Controller
+    {
+        //
+        // GET: /DanielNavarrete/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
